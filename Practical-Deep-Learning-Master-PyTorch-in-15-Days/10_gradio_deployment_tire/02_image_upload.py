74693877f0f697dae2a6c1a71a9dc43bb08875c7
seshu1729 <seshu123000@gmail.com>
2025-09-09 06:32:24 +0000

    Final commit

---
+++ Practical-Deep-Learning-Master-PyTorch-in-15-Days/10_gradio_deployment_tire/02_image_upload.py
@@ -0,0 +1,21 @@
+# gradio 02_image_upload.py
+import gradio as gr
+from PIL import Image
+
+def greet(name, image_pixels):
+    print(type(image_pixels))
+    print(image_pixels.shape)
+
+    img = Image.fromarray(image_pixels)
+    # img.save("uploaded.jpg")
+    print(img)
+
+    return "Hello " + name + "!"
+
+demo = gr.Interface(fn=greet, 
+                    inputs=[
+                        gr.Text(label="Please enter your name:"),
+                        gr.Image()
+                    ], 
+                    outputs=gr.Text(label="Output:"))
+demo.launch() 
---
+++ Practical-Deep-Learning-Master-PyTorch-in-15-Days/data/09_Images/Readme.md
@@ -0,0 +1,4 @@
+https://www.kaggle.com/datasets/warcoder/tyre-quality-classification
+
+
+Download the dataset from the above link.
